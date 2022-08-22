01c6ae8cb798ee6094f8502e82255a0dbbbdc36c
under-44 <benhootnz@gmail.com>
2022-08-23 06:03:44 +1200

    Another function testing ownership
    testing description again
    allo from here

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 fn main() 
 {
     println!("{}", two_literals_to_string("Danniel", "Radcliffe"));
+    println!("{}", add_char_to_string(String::from("Danniel "), 'G'));
+  
+    let testing = String::from("Hello, world");
+    println!("{}", add_char_to_string(testing, '!'));
 }
 
 
 ///### adds two literals with format
-///returns String type
+///```
+/// return String
+///```
+/// 
 fn two_literals_to_string(string1: &str, string2: &str) -> String
 {
     format!("{} - {}", string1, string2)
+}
+
+
+///### adds a char onto end of String
+///```
+///return String 
+///```
+fn add_char_to_string(mut my_string: String, my_char: char) -> String
+{
+    my_string.push(my_char);
+    my_string   
 }
