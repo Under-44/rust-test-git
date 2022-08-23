f97af439b18a5f7aed338902b101dc03445898ac
under-44 <benhootnz@gmail.com>
2022-08-23 12:45:56 +1200

    Added one more test, cleaned up the code :joy: this was a emoji test í±

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-fn main() 
-{
-    println!("{}", two_literals_to_string("Danniel", "Radcliffe"));
-    println!("{}", add_char_to_string(String::from("Danniel "), 'G'));
+// i'm testing with &str and String and
+// using git to get familier with syntax
+fn main()
+{    
+    println!("{}", two_literals_to_string("Danniel", "Radcliffe"));         //these are just some little tests for the
+    println!("{}", add_char_to_string(String::from("Danniel "), 'G'));      //two fuctions in here. just testing how
+    println!("{}", add_char_to_string("Jason ".to_owned(), 'T'));           //ownership works and that jazz
   
     let testing = String::from("Hello, world");
     println!("{}", add_char_to_string(testing, '!'));
@@ -26,5 +29,5 @@ fn two_literals_to_string(string1: &str, string2: &str) -> String
 fn add_char_to_string(mut my_string: String, my_char: char) -> String
 {
     my_string.push(my_char);
-    my_string   
+    my_string
 }
